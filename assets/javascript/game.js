e143904da4774b9311df4891c5b3ac319ad58db1
Don Zarrillo Jr <donzarrillo@gmail.com>
2018-06-15 02:18:15 -0400

    Add files via upload
    Added additional logic to check for non alphanumeric strings

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+var winsLabel = document.getElementById("wins");
+var losssLabel = document.getElementById("losses");
+var guessesleftLabel = document.getElementById("guessesleft");
+var yourguessesLabel = document.getElementById("yourguesses");
+var messageLabel = document.getElementById("message");
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var guessesLeft = 9;
+var myWins = 0;
+var myLosses = 0;
+var yourGuesses = "";
+
+
+// Generate a random letter
+function randomLetter(){
+
+   return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+}
+
+console.log("Alphabet " + randomLetter());
+
+document.onkeyup = function (event) {
+    if(guessesLeft > 1) {
+        var letter = event.key.toUpperCase();  
+        var ranLetter = "A" //randomLetter();
+   
+        // Use regex to check for alphanumeric values
+        if (/^[A-Z]/.test(letter)) {
+            messageLabel.style.display = "none"   
+            console.log("hide " + letter);
+            // code
+        
+            if(letter === ranLetter) {
+                winsLabel.innerText = myWins += 1;
+                guessesLeft = 9;
+                guessesleftLabel.innerText = guessesLeft;
+                yourGuesses = "";
+            } else {
+                guessesLeft -= 1;
+                yourguessesLabel.innerText = "";
+                yourGuesses += letter + " ";
+            }
+                    
+         } else {
+            messageLabel.style.display = "block"
+            console.log("block" + letter);
+         }
+
+        
+    } else {
+        // You lost now populate Losses
+        myLosses += 1;
+        // reset Guesses left
+        guessesLeft = 9;
+        // reset your guesses so far
+        yourGuesses = "";
+        losssLabel.innerText = myLosses;
+    }
+    guessesleftLabel.innerText = guessesLeft;
+    yourguessesLabel.innerText = yourGuesses;
+}
